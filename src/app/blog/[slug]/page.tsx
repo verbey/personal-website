9b288fe5cf22efbffa331229fb2b3baf5d7b75d5
Verbiturum <143966105+verbey@users.noreply.github.com>
2025-02-16 20:14:54 +0200

    Add page to display individual posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,51 @@
 import styles from './page.module.css';
+import fs from 'fs';
+import graymatter from 'gray-matter';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 
-export default function Post() {
-	return <div className={styles.page}>Blog Post</div>;
+function getPost(postName: string): {
+	slug: string;
+	frontmatter: {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		[key: string]: any;
+	};
+	content: string;
+} {
+	const markdownWithMeta = fs.readFileSync(
+		`public/posts/${postName}.md`,
+		'utf-8'
+	);
+
+	const { data: frontmatter, content } = graymatter(markdownWithMeta);
+
+	return {
+		slug: postName,
+		frontmatter,
+		content,
+	};
+}
+
+export default async function Post({ params }: { params: { slug: string } }) {
+	const { slug } = await params;
+	const post = getPost(slug);
+	return (
+		<div className={styles.page}>
+			<header>
+				<h1>{post.frontmatter.title}</h1>
+				<time dateTime={post.frontmatter.date}>
+					{new Date(post.frontmatter.date).toLocaleDateString()}
+				</time>
+				<ul>
+					{post.frontmatter.tags.map((tag: string, index: number) => (
+						<li key={index}>{tag}</li>
+					))}
+				</ul>
+			</header>
+			<main>
+				<article>
+					<MDXRemote source={post.content} />
+				</article>
+			</main>
+		</div>
+	);
 }
