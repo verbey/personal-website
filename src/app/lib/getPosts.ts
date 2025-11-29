50134ec9fe8318f4257d1f6f870d76b05ff2193d
verbey <bivanestor@tutanota.com>
2025-11-29 07:22:47 +0000

    Remove unnecessary comment

--- src/app/lib/getPosts.ts
+++ src/app/lib/getPosts.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import matter from 'gray-matter'; // I assume you use gray-matter
+import matter from 'gray-matter';
 
 const postsDir = path.join(process.cwd(), 'src/app/posts');
 
