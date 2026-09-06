bef4b8d991b22ec1fd6d89116ba0043f828c4d30
verbey <bivanestor@tutanota.com>
2026-09-06 18:53:30 +0000

    Add Logo component

---
+++ src/lib/components/Logo/Halo.svelte
@@ -0,0 +1,12 @@
+<img src="halo.webp" alt="Halo" class="halo" />
+
+<style>
+    .halo {
+        height: 2rem;
+        width: auto;
+        aspect-ratio: 1/1;
+        position: relative;
+        right: 1.5rem;
+        z-index: 1;
+    }
+</style>
---
+++ src/lib/components/Logo/Logo.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    // @ts-expect-error Svelte component imports use a non-TypeScript extension.
+    import Halo from "$lib/components/Logo/Halo.svelte";
+</script>
+
+<div class="logoContainer">
+    <h1 class="logoText">Victor Cherkashyn</h1>
+    <Halo />
+</div>
+
+<style>
+    .logoContainer {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .logoText {
+        font-size: 2rem;
+        margin: 0;
+        padding: 0;
+        z-index: 2;
+    }
+
+    @media screen and (max-width: 360px) {
+        .logoText {
+            font-size: 1.5rem;
+        }
+        .logoContainer {
+            justify-content: flex-start;
+        }
+    }
+
+    @media screen and (max-width: 280px) {
+        .logoText {
+            font-size: 1rem;
+        }
+    }
+</style>
