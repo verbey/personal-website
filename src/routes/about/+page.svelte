c8a42c2ae12393665b3c03e682ca95ed4ff36e2b
verbey <bivanestor@tutanota.com>
2026-09-08 15:21:41 +0000

    Add a simple About me page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<p>
+    My name is Victor Cherkashyn. I'm an IT support specialist based in Lviv,
+    Ukraine. I spend my time learning, experimenting, and writing code because I
+    enjoy it. Other interests include writing essays, reading books and
+    traveling when possible.
+</p>
+
+<p>
+    I'm interested in how software works at different levels, from web
+    applications to lower-level systems, and in how security and privacy fit
+    into everyday tools. I tend to prefer simple, understandable solutions and
+    software that respects users rather than trying to extract as much data as
+    possible I care about free software not as an ideology, but as a practical
+    way to build better, more transparent tools.
+</p>
