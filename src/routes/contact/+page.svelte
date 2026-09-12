ab249306e6326dbd008ebb6bb7e5ecf1f537e3de
verbey <bivanestor@tutanota.com>
2026-09-12 10:49:45 +0000

    Add contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Link from "$lib/components/elements/Link.svelte";
+</script>
+
+<p>
+    There's a myriad of ways you can reach out to me. Feel free to pick any that
+    you like better:
+</p>
+
+<ul>
+    <li>
+        Email: <Link href="mailto:victor@cherkashyn.menubar"
+            >victor@cherkashyn.me</Link
+        >
+    </li>
+    <li>
+        Matrix: <Link href="https://matrix.to/#/@mourtis:flip.earth"
+            >@mourtis:flip.earth</Link
+        >
+    </li>
+    <li>XMPP: @vittorio_soprano:tchncs.de</li>
+</ul>
